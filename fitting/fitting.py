713eac0998ba59601a41ba0d6820d2b7cd4ad445
HayatoTanoue <hayatotanoue7321@gmail.com>
2020-10-31 13:39:18 +0900

    edit docstring

--- fitting/fitting.py
+++ fitting/fitting.py
@@ -8,27 +8,27 @@ from scipy.optimize import curve_fit
 
 __all__ = ["degree_distribution","degree_correlation"]
 
-def exp_func(x, a, b):
+def _exp_func(x, a, b):
     """冪乗関数"""
     return b*(x**a)
 
-def exp_fit(val1_quan, val2_quan):
+def _exp_fit(val1_quan, val2_quan):
     """
     累乗フィッティングを行う関数
     
     Parameters
     ----------
-    val1_quan : list
+    val1_quan : numpy ndarray
     
-    val2_quan : list
+    val2_quan : numpy ndarray
     
     --------------------
     return : numpy ndarray
             冪乗フィッティングの係数bと指数a
     """
     # maxfev：関数の呼び出しの最大数, check_finite：Trueの場合NaNが含まれている場合はValueError発生
-    l_popt, l_pcov = curve_fit(exp_func, val1_quan, val2_quan, maxfev=10000, check_finite=False)
-    return exp_func(val1_quan, *l_popt), l_popt
+    l_popt, l_pcov = curve_fit(_exp_func, val1_quan, val2_quan, maxfev=10000, check_finite=False)
+    return _exp_func(val1_quan, *l_popt), l_popt
 
 def exp_plot(array_x, array_y, y_fit, l_popt, save_name=None):
     """
@@ -56,7 +56,7 @@ def exp_plot(array_x, array_y, y_fit, l_popt, save_name=None):
     ax.scatter(array_x, array_y, color='lime') #散布図
     ax.plot(array_x, y_fit, label='model', color='magenta') #近似直線
     #近似の指数を表示
-    plt.text(1.0, 1.0, 'gamma={}'.format(l_popt[0]) ,ha='right', va='top',transform=ax.transAxes)
+    plt.text(1.0, 1.0, 'a ={}'.format(l_popt[0]) ,ha='right', va='top',transform=ax.transAxes)
     #両軸を対数に
     plt.gca().set_xscale("log")
     plt.gca().set_yscale("log")
@@ -93,7 +93,7 @@ def degree_distribution(G, plot=False, save_name=None):
     #出現回数を割合に変換
     array_cnt = np.array(cnt) / nx.number_of_nodes(G)
     
-    y_fit, l_popt = exp_fit( array_k, array_cnt)
+    y_fit, l_popt = _exp_fit( array_k, array_cnt)
     
     print('a : {},   b : {}'.format(l_popt[0],l_popt[1]))#求めたパラメータa,bを確認
     
@@ -138,7 +138,7 @@ def degree_correlation(G, plot=False, save_name=None):
 
     array_k = np.array( [k[0] for k in average_k_nn] )
     array_k_nn = np.array([k[1] for k in average_k_nn])
-    y_fit,l_popt = exp_fit(array_k, array_k_nn)
+    y_fit,l_popt = _exp_fit(array_k, array_k_nn)
 
     print('a : {},   b : {}'.format(l_popt[0],l_popt[1]))#求めたパラメータa,bを確認
     
