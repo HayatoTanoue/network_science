24fc8f22c601005e33f17bc508c117fc37a10a9f
HayatoTanoue <hayatotanoue7321@gmail.com>
2020-11-01 13:18:41 +0900

    edit

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-import network_science.fitting
-from network_science.fitting import *
 import network_science.chapter5
 from network_science.chapter5 import *
+
+import network_science.fitting
+from network_science.fitting import *
