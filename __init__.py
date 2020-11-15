32fbc4a63950b03ecd595fa0fc7630e5bb1da91f
HayatoTanoue <hayatotanoue7321@gmail.com>
2020-11-15 16:35:24 +0900

    add description

--- __init__.py
+++ __init__.py
@@ -1,3 +1,17 @@
+
+'''
+    network_science
+    =====
+    Network Science(barabasi-albert) 内の便利そうなものをゆったり実装
+
+    Repository: https://github.com/HayatoTanoue/network_science
+    License: MIT
+'''
+
+__author__ = 'Hayato Tanoue'
+__version__ = '0.1'
+__license__ = 'MIT'
+
 import network_science.chapter5
 from network_science.chapter5 import *
 
