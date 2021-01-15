a5d5e454d7f9b5facb6749bcd7b02a78fc158ac0
HayatoTanoue <hayatotanoue7321@gmail.com>
2021-01-15 16:04:30 +0900

    indent

--- network_science/chapter5/modelA_B.py
+++ network_science/chapter5/modelA_B.py
@@ -4,10 +4,11 @@ import numpy as np
 
 __all__ = ["no_preferential_attachment", "no_growth_barabasi"]
 
+
 def no_preferential_attachment(n, m):
     """
     優先的選択がないバラバシアルバートグラフ
-    
+
     network schience chapter5.6 model A
 
     Parameters
@@ -22,24 +23,25 @@ def no_preferential_attachment(n, m):
     G : networkx graph
 
     """
-    
-    G = nx.empty_graph(m) #generate m nodes empty graphe
-    
+
+    G = nx.empty_graph(m)  # generate m nodes empty graphe
+
     for i in range(n - m):
         nodes = list(G.nodes())
         new_node = nodes[-1] + 1
         G.add_node(new_node)
-        
+
         selected = np.random.choice(nodes, size=m, replace=False)
-        
+
         G.add_edges_from([(new_node, select) for select in selected])
-        
+
     return G
+
 
 def no_growth_barabasi(n, step):
     """
     成長のないバラバシアルバートグラフ
-    
+
     network schience chapter5.6 model B
 
     Parameters
@@ -53,28 +55,28 @@ def no_growth_barabasi(n, step):
     -------
     G : networx graph
     """
-    G = nx.empty_graph(n) #generate n nodes empty graphe
-    
+    G = nx.empty_graph(n)  # generate n nodes empty graphe
+
     def select_by_degree(G):
         """ 次数に比例した選択確率によるノード抽選 """
-        degree = [G.degree(i) + 1 for i in range(n)] #次数0 でも抽選されるように+1を行う
-        total_degree = sum(degree) 
-        weight = [i/total_degree for i in degree]
+        degree = [G.degree(i) + 1 for i in range(n)]  # 次数0 でも抽選されるように+1を行う
+        total_degree = sum(degree)
+        weight = [i / total_degree for i in degree]
         selected = np.random.choice(range(n), p=weight)
-        
+
         return selected
-    
+
     s = 0
     while s < step:
-        #各ステップで無作為に抽選したノードからエッジを一つ張る
+        # 各ステップで無作為に抽選したノードからエッジを一つ張る
         now = np.random.randint(n)
-        
+
         selected = select_by_degree(G)
         while now == selected:
             selected = select_by_degree(G)
-        
+
         G.add_edge(now, selected)
-        
+
         s += 1
-    
+
     return G
--- network_science/fitting/fitting.py
+++ network_science/fitting/fitting.py
@@ -7,22 +7,24 @@ import numpy as np
 import seaborn as sns
 from scipy.optimize import curve_fit
 
-__all__ = ["degree_distribution","degree_correlation"]
+__all__ = ["degree_distribution", "degree_correlation"]
+
 
 def _exp_func(x, a, b):
     """冪乗関数"""
-    return b*(x**a)
+    return b * (x**a)
+
 
 def _exp_fit(val1_quan, val2_quan):
     """
     累乗フィッティングを行う関数
-    
+
     Parameters
     ----------
     val1_quan : numpy ndarray
-    
+
     val2_quan : numpy ndarray
-    
+
     --------------------
     return : numpy ndarray
             冪乗フィッティングの係数bと指数a
@@ -31,55 +33,57 @@ def _exp_fit(val1_quan, val2_quan):
     l_popt, l_pcov = curve_fit(_exp_func, val1_quan, val2_quan, maxfev=10000, check_finite=False)
     return _exp_func(val1_quan, *l_popt), l_popt
 
+
 def exp_plot(array_x, array_y, y_fit, l_popt, save_name=None):
     """
     冪乗プロット, フィッティングの可視化
-    
+
     Parameters
     ----------
     array_x : numpy ndarray
         x 軸の値
-        
+
     array_y : numpy ndarray
         y 軸の値
-    
+
     y_fit : numpy ndarray
         フィッティング結果のy
-    
+
     l_popt : list
         係数a, b
-    
+
     save_name : string
         プロットを保存する場合の保存名
     """
     sns.set()
-    ax=plt.subplot(1,1,1)
-    ax.scatter(array_x, array_y, color='lime') #散布図
-    ax.plot(array_x, y_fit, label='model', color='magenta') #近似直線
-    #近似の指数を表示
-    plt.text(1.0, 1.0, 'a ={}'.format(l_popt[0]) ,ha='right', va='top',transform=ax.transAxes)
-    #両軸を対数に
+    ax = plt.subplot(1, 1, 1)
+    ax.scatter(array_x, array_y, color='lime')  # 散布図
+    ax.plot(array_x, y_fit, label='model', color='magenta')  # 近似直線
+    # 近似の指数を表示
+    plt.text(1.0, 1.0, 'a ={}'.format(l_popt[0]), ha='right', va='top', transform=ax.transAxes)
+    # 両軸を対数に
     plt.gca().set_xscale("log")
     plt.gca().set_yscale("log")
 
-    plt.gca().set_ylim(top=1) #y軸の最大を1に固定
-    
+    plt.gca().set_ylim(top=1)  # y軸の最大を1に固定
+
     if save_name:
         plt.savefig(save_name + ".png")
-    
+
     plt.show()
+
 
 def degree_distribution(G, plot=False, save_name=None):
     """
     次数分布の冪乗フィッティング
     y = b x x ^ a
-    
+
     input:
         G : networkx graph
-        
+
         plot : bool 
             冪乗分布とフィッティング結果のグラフの表示の有無
-        
+
         save_name : string
             プロットを保存する場合の保存名
     --------------------
@@ -89,32 +93,33 @@ def degree_distribution(G, plot=False, save_name=None):
     """
     degree_count = collections.Counter(sorted([d for n, d in G.degree()], reverse=True))
     deg, cnt = zip(*degree_count.items())
-    
+
     array_k = np.array(deg)
-    #出現回数を割合に変換
+    # 出現回数を割合に変換
     array_cnt = np.array(cnt) / nx.number_of_nodes(G)
-    
-    y_fit, l_popt = _exp_fit( array_k, array_cnt)
-    
-    print('a : {},   b : {}'.format(l_popt[0],l_popt[1]))#求めたパラメータa,bを確認
-    
-    #plot
+
+    y_fit, l_popt = _exp_fit(array_k, array_cnt)
+
+    print('a : {},   b : {}'.format(l_popt[0], l_popt[1]))  # 求めたパラメータa,bを確認
+
+    # plot
     if plot:
         exp_plot(array_k, array_cnt, y_fit, l_popt, save_name)
-    
+
     return l_popt[0], l_popt[1]
+
 
 def degree_correlation(G, plot=False, save_name=None):
     """
     次数相関の冪乗フィッティング
     y = b x x ^ a
-    
+
     input:
         G : networkx graph
-        
+
         plot : bool
             冪乗分布とフィッティング結果のグラフの表示の有無
-        
+
         save_name : string
             プロットを保存する場合の保存名
     --------------------
@@ -122,28 +127,28 @@ def degree_correlation(G, plot=False, save_name=None):
             l_popt[0] : 冪乗の指数 a
             l_popt[1] : 冪乗の係数 b  
     """
-    #次数リスト
+    # 次数リスト
     degree = dict(G.degree())
 
-    #近傍ノードの平均次数
+    # 近傍ノードの平均次数
     neighbor_ave_degree = nx.average_neighbor_degree(G)
 
-    #各次数の出現回数
-    degree_count = collections.Counter(sorted( degree.values(), reverse=True))
+    # 各次数の出現回数
+    degree_count = collections.Counter(sorted(degree.values(), reverse=True))
 
-    #knn(ki) 次数相関
+    # knn(ki) 次数相関
     average_k_nn = []
     for k in degree_count.keys():
         node_nums = [num for num in G.nodes() if degree[num] == k]
-        average_k_nn.append((k, np.average([neighbor_ave_degree[i] for i in node_nums])) )
+        average_k_nn.append((k, np.average([neighbor_ave_degree[i] for i in node_nums])))
 
-    array_k = np.array( [k[0] for k in average_k_nn] )
+    array_k = np.array([k[0] for k in average_k_nn])
     array_k_nn = np.array([k[1] for k in average_k_nn])
-    y_fit,l_popt = _exp_fit(array_k, array_k_nn)
+    y_fit, l_popt = _exp_fit(array_k, array_k_nn)
 
-    print('a : {},   b : {}'.format(l_popt[0],l_popt[1]))#求めたパラメータa,bを確認
-    
+    print('a : {},   b : {}'.format(l_popt[0], l_popt[1]))  # 求めたパラメータa,bを確認
+
     if plot:
         exp_plot(array_k, array_k_nn, y_fit, l_popt, save_name)
-    
+
     return l_popt[0], l_popt[1]
